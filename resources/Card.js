18952fc63620eec554f677c79996fa91b51105ac
FazidSamoon <90629392+FazidSamoon@users.noreply.github.com>
2022-01-10 20:23:44 +0530

    Add files via upload

---
+++ resources/Card.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Card(props){
+    return(
+        <div className="card">
+            <img src={`../images/${props.coverImg}`} className="cardImg" />
+
+            <div className="cardDetails">
+            <img src="../images/star.png" className="card--star" />
+                <span>{props.stats.rating}</span>
+                <span className="gray">({props.stats.reviewCount}) • </span>
+                <span className="gray">{props.location}</span>
+            </div>
+
+            <p className="card--title">{props.title}</p>
+            <p className="card--price">
+                <span className="bold">From ${props.price}</span> / person
+            </p>
+        </div>
+    )
+}
---
+++ resources/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Navbar(){
+    return(
+        <nav>
+            <img src="../../pictures/logo.png" />
+            <h1>Navbar</h1>
+        </nav>
+    )
+}
